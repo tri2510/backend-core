88d38838080039e5adcc72cd04924425bf3b8a3b
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-05-20 17:57:18 +0700

    Update media route

--- src/routes/v1/media.route.js
+++ src/routes/v1/media.route.js
@@ -20,4 +20,40 @@ router.get('/:tenantId', async (req, res) => {
   }
 });
 
+router.post('/:tenantId', async (req, res) => {
+  try {
+    const { tenantId } = req.params;
+    const { media } = req.body;
+    const currMedia = (await db.collection('media').doc(tenantId).get()).data() || {};
+    currMedia.media = { ...currMedia?.media, ...JSON.parse(media) };
+
+    console.log('currMedia', currMedia.media);
+
+    await db.collection('media').doc(tenantId).set({
+      media: currMedia.media,
+    });
+    res.send('success');
+  } catch (error) {
+    res.status(400).send('error');
+    // eslint-disable-next-line no-console
+    console.log('error', error);
+  }
+});
+
+router.delete('/:tenantId/:filename', async (req, res) => {
+  try {
+    const { tenantId, filename } = req.params;
+    const currMedia = (await db.collection('media').doc(tenantId).get()).data();
+    delete currMedia?.media[filename];
+    await db.collection('media').doc(tenantId).set({
+      media: currMedia?.media,
+    });
+    res.send('success');
+  } catch (error) {
+    res.status(400).send('error');
+    // eslint-disable-next-line no-console
+    console.log('error', error);
+  }
+});
+
 module.exports = router;
