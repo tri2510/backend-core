54a83f679f3cc1f268f151ba2dd54dd81f5ce1a6
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-07-02 17:07:19 +0700

    add --no-cache args

--- start.sh
+++ start.sh
@@ -36,11 +36,23 @@ echo "Setting permissions for $UPLOAD_PATH"
 sudo chown -R 1000:1000 "$UPLOAD_PATH"
 sudo chmod -R 775 "$UPLOAD_PATH"
 
+# Initialize variables
+DOCKER_COMMAND=""
+NO_CACHE=""
+
+# Check for --no-cache argument
+for arg in "$@"
+do
+  if [ "$arg" == "--no-cache" ]; then
+    NO_CACHE="--no-cache"
+  fi
+done
+
 # Determine the command to run based on the argument
 if [ "$1" == "-prod" ]; then
-  DOCKER_COMMAND="docker compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d"
+  DOCKER_COMMAND="docker compose -f docker-compose.yml -f docker-compose.prod.yml up --build $NO_CACHE -d"
 else
-  DOCKER_COMMAND="docker compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d"
+  DOCKER_COMMAND="docker compose -f docker-compose.yml -f docker-compose.dev.yml up --build $NO_CACHE -d"
 fi
 
 # Run Docker Compose
