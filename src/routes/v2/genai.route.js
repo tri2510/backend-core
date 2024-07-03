0e12ded7901ca5839ffb7dfa5517406fee10cd45
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-07-03 15:35:23 +0700

    fix firebase vgalidation check

--- src/routes/v2/genai.route.js
+++ src/routes/v2/genai.route.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 const express = require('express');
 const mongoose = require('mongoose');
-const { getAuth } = require('firebase-admin/auth');
 const httpStatus = require('http-status');
+const { auth: firebaseAuth } = require('../../config/firebase');
 const auth = require('../../middlewares/auth');
 const { permissionService } = require('../../services');
 const { PERMISSIONS } = require('../../config/roles');
 const ApiError = require('../../utils/ApiError');
 const catchAsync = require('../../utils/catchAsync');
+const logger = require('../../config/logger');
 
 const router = express.Router();
 
@@ -44,6 +45,10 @@ const getAccessToken = async () => {
 };
 
 const hasGenAIPermission = async (userId) => {
+  if (!userId) {
+    return false;
+  }
+
   if (mongoose.Types.ObjectId.isValid(userId)) {
     try {
       return await permissionService.hasPermission(userId, PERMISSIONS.GENERATIVE_AI);
@@ -53,24 +58,25 @@ const hasGenAIPermission = async (userId) => {
   }
 
   try {
-    const record = await getAuth().getUser(userId);
+    const record = (await firebaseAuth.getUser(userId)).toJSON();
+
     if (
       record &&
-      record.email in
-        [
-          'nhan.luongnguyen@vn.bosch.com',
-          'phong.phamtuan@vn.bosch.com',
-          'tam.thaihoangminh@vn.bosch.com',
-          'hoang.phanthanh@vn.bosch.com',
-          'dirk.slama@bosch.com',
-          'thanh.hoang.bk@gmail.com',
-          'tuan.hoangdinhanh@vn.bosch.com',
-          'hdatdragon2@gmail.com',
-        ]
+      [
+        'nhan.luongnguyen@vn.bosch.com',
+        'phong.phamtuan@vn.bosch.com',
+        'tam.thaihoangminh@vn.bosch.com',
+        'hoang.phanthanh@vn.bosch.com',
+        'dirk.slama@bosch.com',
+        'thanh.hoang.bk@gmail.com',
+        'tuan.hoangdinhanh@vn.bosch.com',
+        'hdatdragon2@gmail.com',
+      ].includes(record.email)
     ) {
       return true;
     }
   } catch (error) {
+    logger.error(error);
     return false;
   }
   return false;
@@ -78,7 +84,7 @@ const hasGenAIPermission = async (userId) => {
 
 const generateAIContent = catchAsync(async (req, res) => {
   const { prompt, user } = req.body;
-  if (!(await hasGenAIPermission(user || req.user.id))) {
+  if (!(await hasGenAIPermission(user || req.user?.id))) {
     throw new ApiError(httpStatus.FORBIDDEN, 'You do not have permission to use GenAI service');
   }
   try {
@@ -106,6 +112,12 @@ const generateAIContent = catchAsync(async (req, res) => {
   }
 });
 
-router.post('/', auth(), generateAIContent);
+router.post(
+  '/',
+  auth({
+    optional: true,
+  }),
+  generateAIContent
+);
 
 module.exports = router;
