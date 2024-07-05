561276d6d10b9293443a0aa25baa81f617af6abf
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-07-05 14:21:53 +0700

    Fix nullable req.user

--- src/routes/v2/search.route.js
+++ src/routes/v2/search.route.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { searchController } = require('../../controllers');
 const { searchValidation } = require('../../validations');
 const validate = require('../../middlewares/validate');
+const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/', validate(searchValidation.search), searchController.search);
+router.get('/', auth({ optional: true }), validate(searchValidation.search), searchController.search);
 
 module.exports = router;
