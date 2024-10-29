dc5876daf5c1c9aead739b1e85250bc7d13d7dd9
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-10-29 16:55:27 +0700

    add timestamps for prototypes model

--- src/models/prototype.model.js
+++ src/models/prototype.model.js
@@ -81,124 +81,129 @@ const ratingSchema = mongoose.Schema({
   },
 });
 
-const prototypeSchema = mongoose.Schema({
-  apis: {
-    type: apiSchema,
-    default: {
-      VSC: [],
-      VSS: [],
+const prototypeSchema = mongoose.Schema(
+  {
+    apis: {
+      type: apiSchema,
+      default: {
+        VSC: [],
+        VSS: [],
+      },
+    },
+    code: {
+      type: String,
+      required: true,
+      default:
+        'from sdv_model import Vehicle import plugins from browser import aio vehicle = Vehicle() # write your code here',
+    },
+    extend: {
+      type: mongoose.SchemaTypes.Mixed,
+    },
+    complexity_level: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+      default: 3,
+    },
+    customer_journey: {
+      type: String,
+      required: true,
+      default:
+        ' #Step 1 Who: Driver What: Wipers turned on manually Customer TouchPoints: Windshield wiper switch #Step 2 Who: User What: User opens the car door/trunk and the open status of door/trunk is set to true Customer TouchPoints: Door/trunk handle #Step 3 Who: System What: The wiping is immediately turned off by the software and user is notified Customer TouchPoints: Notification on car dashboard and mobile app ',
+    },
+    description: {
+      type: descriptionSchema,
+      default: {
+        problem: '',
+        says_who: '',
+        solution: '',
+        status: '',
+      },
+    },
+    image_file: {
+      type: String,
+    },
+    journey_image_file: {
+      type: String,
+    },
+    analysis_image_file: {
+      type: String,
+    },
+    model_id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Model',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 255,
+    },
+    portfolio: {
+      type: portfolioSchema,
+      default: {
+        effort_estimation: 0,
+        needs_addressed: 0,
+        relevance: 0,
+      },
+    },
+    skeleton: {
+      type: String,
+    },
+    state: {
+      type: String,
+      required: true,
+      default: stateTypes.DEVELOPMENT,
+      enums: Object.values(stateTypes),
+    },
+    tags: {
+      type: [tagSchema],
+    },
+    widget_config: {
+      type: String,
+    },
+    last_viewed: {
+      type: Date,
+    },
+    rated_by: {
+      type: Map,
+      of: ratingSchema,
+      default: {},
+    },
+    autorun: {
+      type: Boolean,
+      default: false,
+    },
+    related_ea_components: {
+      type: String,
+    },
+    partner_logo: {
+      type: String,
+    },
+    created_by: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    executed_turns: {
+      type: Number,
+      default: 0,
+    },
+    language: {
+      type: String,
+      default: 'python',
+      maxLength: 20,
+    },
+    requirements: {
+      type: String,
     },
   },
-  code: {
-    type: String,
-    required: true,
-    default:
-      'from sdv_model import Vehicle import plugins from browser import aio vehicle = Vehicle() # write your code here',
-  },
-  extend: {
-    type: mongoose.SchemaTypes.Mixed,
-  },
-  complexity_level: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-    default: 3,
-  },
-  customer_journey: {
-    type: String,
-    required: true,
-    default:
-      ' #Step 1 Who: Driver What: Wipers turned on manually Customer TouchPoints: Windshield wiper switch #Step 2 Who: User What: User opens the car door/trunk and the open status of door/trunk is set to true Customer TouchPoints: Door/trunk handle #Step 3 Who: System What: The wiping is immediately turned off by the software and user is notified Customer TouchPoints: Notification on car dashboard and mobile app ',
-  },
-  description: {
-    type: descriptionSchema,
-    default: {
-      problem: '',
-      says_who: '',
-      solution: '',
-      status: '',
-    },
-  },
-  image_file: {
-    type: String,
-  },
-  journey_image_file: {
-    type: String,
-  },
-  analysis_image_file: {
-    type: String,
-  },
-  model_id: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'Model',
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength: 255,
-  },
-  portfolio: {
-    type: portfolioSchema,
-    default: {
-      effort_estimation: 0,
-      needs_addressed: 0,
-      relevance: 0,
-    },
-  },
-  skeleton: {
-    type: String,
-  },
-  state: {
-    type: String,
-    required: true,
-    default: stateTypes.DEVELOPMENT,
-    enums: Object.values(stateTypes),
-  },
-  tags: {
-    type: [tagSchema],
-  },
-  widget_config: {
-    type: String,
-  },
-  last_viewed: {
-    type: Date,
-  },
-  rated_by: {
-    type: Map,
-    of: ratingSchema,
-    default: {},
-  },
-  autorun: {
-    type: Boolean,
-    default: false,
-  },
-  related_ea_components: {
-    type: String,
-  },
-  partner_logo: {
-    type: String,
-  },
-  created_by: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  executed_turns: {
-    type: Number,
-    default: 0,
-  },
-  language: {
-    type: String,
-    default: 'python',
-    maxLength: 20,
-  },
-  requirements: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // add plugin that converts mongoose to json
 prototypeSchema.plugin(toJSON);
