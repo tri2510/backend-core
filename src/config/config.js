ff40c527c74e09740f4f0937ca1566e399979bc2
tuan-hda <anhtuan.hoangdinh@gmail.com>
2025-04-08 10:29:02 +0700

    refactor: removing the requirement of JWT Secret

--- src/config/config.js
+++ src/config/config.js
@@ -12,7 +12,7 @@ const envVarsSchema = Joi.object()
     // CORS Settings
     CORS_ORIGIN: Joi.string().description('CORS regex'),
     // JWT
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
+    JWT_SECRET: Joi.string().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
     JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
